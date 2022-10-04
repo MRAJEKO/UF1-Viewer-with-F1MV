88ded574879e25f017b5bf2dcd7ce8b89e95dd0d
Aiden Kops <6030509@mborijnland.nl>
2022-10-04 22:35:16 +0200

    Restored to countdowns

--- resources/app/src/main/refer.js
+++ resources/app/src/main/refer.js
@@ -34,33 +34,3 @@ function countdowns() {
         "height=100px,width=400px,transparent=true,frame=false,resizable=true,hasShadow=false,webPreferences={devTools=false},top=0,left=200,nodeIntegration=no"
     );
 }
-
-function singleRCM() {
-    var left = (window.screen.width - 1000) / 2;
-    console.log(left);
-    window.open(
-        "../SingleRCM/index.html",
-        "_blank",
-        "top=0,left=" +
-            left +
-            ",alwaysOnTop=true,width=1000px,height=100px,frame=false,transparent=true,resizable=false,hasShadow=false,webPreferences={devTools=false}"
-    );
-}
-
-function liveData() {
-    window.open(
-        "../LiveData/index.html",
-        "_blank",
-        "top=500,left=200,frame=false,nodeIntegration=no"
-    );
-}
-
-function crashDetection() {
-    window.open(
-        "../CrashDetection/index.html",
-        "_blank",
-        "top=500,left=200,frame=false,nodeIntegration=no"
-    );
-}
-
-function pitStopData() {}
