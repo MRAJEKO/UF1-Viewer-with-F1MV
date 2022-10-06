cc0c39b3d2ed97bafa26020292db17fd5c38350f
Sam R <sam@readis.au>
2022-10-06 21:24:06 +1100

    Ported update/launch script from py to node. Added MVF1 installs to gitignore

--- resources/app/src/MultiViewer/MV.cmd
+++
@@ -1,3 +0,0 @@
-C:/Users/Aiden/AppData/Local/Microsoft/WindowsApps/python3.10.exe C:\"UF1 Viewer with F1MV"\resources\app\src\MultiViewer\mvAutoUpdater.py
-START C:\"UF1 Viewer with F1MV"\resources\app\src\MultiViewer\"MultiViewer for F1"\"MultiViewer for F1.exe"
-EXIT
