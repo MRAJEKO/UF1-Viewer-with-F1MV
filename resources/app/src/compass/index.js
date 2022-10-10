ceaa3a043f37df849cad8b43e0fe7aa3633fe403
MRAJEKO <aiden.kops@outlook.com>
2022-10-10 14:36:36 +0200

    Get and add values

--- resources/app/src/compass/index.js
+++ resources/app/src/compass/index.js
@@ -1,3 +1,24 @@
+function httpGet(theUrl) {
+    let xmlHttpReq = new XMLHttpRequest();
+    xmlHttpReq.open("GET", theUrl, false);
+    xmlHttpReq.send(null);
+    return xmlHttpReq.responseText;
+}
+
+let sessionInfo = JSON.parse(
+    httpGet("http://localhost:10101/api/v1/live-timing/SessionInfo")
+);
+
+let circuitKey = sessionInfo.Meeting.Circuit.Key;
+
+let season = sessionInfo.StartDate.slice(0, 4);
+
+let trackRotation = JSON.parse(
+    httpGet(`https://api.f1mv.com/api/v1/circuits/${circuitKey}/${season}`)
+).rotation;
+
+document.getElementById("compass").style.rotate = trackRotation + "deg";
+
 document.addEventListener("keydown", (event) => {
     if (event.key == "Escape") {
         toggleBackground();
@@ -7,10 +28,10 @@ document.addEventListener("keydown", (event) => {
 let transparent = false;
 function toggleBackground() {
     if (transparent) {
-        document.getElementById("time").className = "";
+        document.getElementById("background").className = "";
         transparent = false;
     } else {
-        document.getElementById("time").className = "transparent";
+        document.getElementById("background").className = "transparent";
         transparent = true;
     }
 }
