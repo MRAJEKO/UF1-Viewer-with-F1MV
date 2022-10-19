ce5a78b8c60f6a463985bf67774c4229dcfe0afb
MRAJEKO <aiden.kops@outlook.com>
2022-10-19 15:27:19 +0200

    Base functionalty

--- resources/app/src/crashDetection/index.js
+++ resources/app/src/crashDetection/index.js
@@ -15,17 +15,17 @@ function httpGet(theUrl) {
 let api;
 let driverList;
 let carData;
-let driverName;
-let teamColor;
+let timingData;
 
 function apiRequests() {
     api = JSON.parse(
         httpGet(
-            "http://localhost:10101/api/v2/live-timing/state/DriverList,CarData"
+            "http://localhost:10101/api/v2/live-timing/state/DriverList,CarData,TimingData"
         )
     );
     driverList = api.DriverList;
     carData = api.CarData;
+    timingData = api.TimingData;
     if (debug) {
         console.log("------------------------------");
         console.log("Driver list:");
@@ -35,18 +35,98 @@ function apiRequests() {
     }
 }
 
-function numberToDriver(number) {
-    driverName =
-        driverList[number].FirstName +
-        " " +
-        driverList[number].LastName.toUpperCase();
-    teamColor = driverList[number].TeamColour;
+function getDriverName(number) {
     if (debug) {
         console.log("------------------------------");
-        console.log("Driver name: " + driverName);
-        console.log("Team color hex: " + teamColor);
+        console.log(
+            "Driver name: " +
+                driverList[number].FirstName +
+                " " +
+                driverList[number].LastName.toUpperCase()
+        );
+    }
+    return (
+        driverList[number].FirstName +
+        " " +
+        driverList[number].LastName.toUpperCase()
+    );
+}
+
+function getTeamColor(number) {
+    if (debug) {
+        console.log("------------------------------");
+        console.log("Team color hex: " + driverList[number].TeamColour);
+    }
+    return driverList[number].TeamColour;
+}
+
+function getCarData(number) {
+    if (debug) {
+        console.log("------------------------------");
+        console.log(carData.Entries[0].Cars[number].Channels);
+    }
+    return carData.Entries[0].Cars[number].Channels;
+}
+
+function getCarStatus(data) {
+    let rpm = data[0];
+    let speed = data[2];
+    let gear = data[3];
+
+    if (rpm === 0 || speed <= 30 || gear > 8 || gear === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+let container = document.getElementById("container");
+
+let count = 0;
+async function run() {
+    while (true) {
+        if (debug) {
+            console.log("------------------------------");
+        }
+        apiRequests();
+        for (i in driverList) {
+            let number = driverList[i].RacingNumber;
+            let name = getDriverName(number);
+            let color = getTeamColor(number);
+            let data = getCarData(number);
+            let crashed = getCarStatus(data);
+            let driverElement = document.getElementById(number);
+            if (crashed) {
+                let driverData = timingData.Lines[number];
+                if (
+                    driverData.InPit === true ||
+                    driverData.Retired === true ||
+                    driverData.Stopped === true
+                ) {
+                    crashed = false;
+                }
+            }
+            if (crashed) {
+                if (driverElement == null) {
+                    let element = `<p id="${number}">${name}</p>`;
+                    container.innerHTML += element;
+                    document.getElementById(number).style.color = "#" + color;
+                }
+                if (debug) {
+                    console.log(name + " has crashed");
+                }
+            } else {
+                if (driverElement == null) {
+                } else {
+                    driverElement.remove();
+                }
+            }
+        }
+
+        await sleep(250);
+        if (debug) {
+            console.log(count++);
+        }
     }
 }
 
-apiRequests();
-numberToDriver("5");
+run();
