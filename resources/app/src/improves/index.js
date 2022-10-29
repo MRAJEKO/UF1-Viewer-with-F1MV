2ffb63167542068d8d7cfb50e6206e1e7da74f2a
MRAJEKO <aiden.kops@outlook.com>
2022-10-29 16:26:25 +0200

    Base JS code

---
+++ resources/app/src/improves/index.js
@@ -0,0 +1,30 @@
+const debug = false;
+
+// Set sleep
+const sleep = (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+function httpGet(theUrl) {
+    let xmlHttpReq = new XMLHttpRequest();
+    xmlHttpReq.open("GET", theUrl, false);
+    xmlHttpReq.send(null);
+    return xmlHttpReq.responseText;
+}
+
+let transparent = false;
+function toggleBackground() {
+    if (transparent) {
+        document.querySelector("body").className = "";
+        transparent = false;
+    } else {
+        document.querySelector("body").className = "transparent";
+        transparent = true;
+    }
+}
+
+document.addEventListener("keydown", (event) => {
+    if (event.key == "Escape") {
+        toggleBackground();
+    }
+});
