269a2ebfd82c82579ee931092d38ef6b09f268f3
MRAJEKO <aiden.kops@outlook.com>
2022-10-30 19:18:48 +0100

    Working without animation

--- resources/app/src/improves/index.js
+++ resources/app/src/improves/index.js
@@ -62,19 +62,115 @@ function setDriverTimes() {
     }
 }
 
+async function showImprovedLap(data) {
+    console.log(data);
+    let container = document.getElementById("container");
+    let length = container.children.length;
+    let newLap = `<div class="header">
+            <div class="position">
+                <p>${data.position}</p>
+            </div>
+            <div class="icon" style="background-color: ${data.teamColor}">
+                <img src="${data.teamIcon}" alt="" />
+            </div>
+            <div class="name">
+                <p>${data.name}</p>
+            </div>
+            <div class="tire">
+                <p style="color: ${data.tireColor}">${data.tire}</p>
+            </div>
+        </div>
+        <div class="times">
+            <div class="personal">
+                <p style="color: ${data.lapTimeColor}">${data.lapTime}</p>
+            </div>
+            <div class="target">
+                <div class="top">
+                    <div class="delta">
+                        <p style="color: ${data.targetDeltaColor}">${data.targetDelta}</p>
+                    </div>
+                    <div class="target-time">
+                        <p style="color: ${data.targetTimeColor}">${data.targetTime}</p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="target-position">
+                        <p>${data.targetPosition}</p>
+                    </div>
+                    <div class="target-name"><p>${data.targetName}</p></div>
+                </div>
+            </div>
+        </div>
+        <div class="sectors">
+            <div id="sector1" class="sector">
+                <div class="sector-times">
+                    <div class="sector-time">
+                        <p>${data.sector1.time}</p>
+                    </div>
+                    <div class="sector-delta">
+                        <p style="color: ${data.sector1.deltaColor}">${data.sector1.delta}</p>
+                    </div>
+                </div>
+                <div
+                    class="sector-color"
+                    style="background-color: ${data.sector1.color}"
+                ></div>
+            </div>
+            <div id="sector2" class="sector">
+                <div class="sector-times">
+                    <div class="sector-time">
+                        <p>${data.sector2.time}</p>
+                    </div>
+                    <div class="sector-delta">
+                        <p style="color: ${data.sector2.deltaColor}">${data.sector2.delta}</p>
+                    </div>
+                </div>
+                <div
+                    class="sector-color"
+                    style="background-color: ${data.sector2.color}"
+                ></div>
+            </div>
+            <div id="sector3" class="sector">
+                <div class="sector-times">
+                    <div class="sector-time">
+                        <p>${data.sector3.time}</p>
+                    </div>
+                    <div class="sector-delta">
+                        <p style="color: ${data.sector3.deltaColor}">${data.sector3.delta}</p>
+                    </div>
+                </div>
+                <div
+                    class="sector-color"
+                    style="background-color: ${data.sector3.color}"
+                ></div>
+            </div>
+        </div>`;
+    let newElement = document.createElement("li");
+    newElement.classList.add("improved-time");
+    newElement.innerHTML = newLap;
+    container.appendChild(newElement);
+    container = document.getElementById("container");
+    await sleep(10);
+    newElement.classList.add("show");
+    if (length >= 5) {
+        let lastLap = container.children[length - 1];
+        lastLap.classList.remove("show");
+        await sleep(400);
+        lastLap.remove();
+    }
+}
+
 async function getImprovedDriver() {
     for (i in timingData) {
         if (
-            timingData[i].LastLapTime.PersonalFastest ||
+            timingData[i].LastLapTime.PersonalFastest &&
             timingData[i].BestLapTime.Value != bestLapTimes[i]
         ) {
             bestLapTimes[i] = timingData[i].BestLapTime.Value;
             let driverData = await getAllDriverData(i);
-            console.log(driverData);
+            await showImprovedLap(driverData);
         }
     }
-
-    console.log(bestLapTimes);
 }
 
 function getSectorInfo(driverNumber, sectorNumber, targetRacingNumber) {
@@ -91,15 +187,15 @@ function getSectorInfo(driverNumber, sectorNumber, targetRacingNumber) {
     if (delta >= 0) {
         delta = "+" + delta;
     } else {
-        deltaColor = "green";
+        deltaColor = "#00ff00";
     }
-    let sectorColor = "green";
+    let sectorColor = "#00ff00";
     if (timingData[driverNumber].Sectors[sectorNumber - 1].OverallFastest) {
-        sectorColor = "purple";
+        sectorColor = "#800080";
     } else if (
         timingData[driverNumber].Sectors[sectorNumber - 1].PersonalFastest
     ) {
-        sectorColor = "green";
+        sectorColor = "#00ff00";
     } else {
         sectorColor = "yellow";
     }
@@ -129,25 +225,33 @@ async function getAllDriverData(racingNumber) {
     let position = timingData[racingNumber].Position;
     driverData["position"] = position;
     driverData["teamIcon"] = "../icons/" + icons[driverList[i].TeamName];
-    driverData["teamColor"] = driverList[i].TeamColour;
+    driverData["teamColor"] = "#" + driverList[i].TeamColour;
     driverData["name"] = driverList[i].LastName.toUpperCase();
     let tire =
         tireData[i].Stints[+tireData[i].Stints.length - 1].Compound.charAt(0);
     driverData["tire"] = tire;
-    let tireColor = "soft";
+    let tireColor = "red";
     if (tire == "H") {
-        tireColor = "hard";
+        tireColor = "white";
     }
     if (tire == "M") {
-        tireColor = "medium";
+        tireColor = "yellow";
     }
     driverData["tireColor"] = tireColor;
     let targetPosition;
+    let lapTimeColor;
+    let targetTimeColor;
     if (position != 1) {
         targetPosition = "P" + 1;
+        lapTimeColor = "#00ff00";
+        targetTimeColor = "#800080";
     } else {
         targetPosition = "P" + 2;
+        lapTimeColor = "#800080";
+        targetTimeColor = "#00ff00";
     }
+    driverData["targetTimeColor"] = targetTimeColor;
+    driverData["lapTimeColor"] = lapTimeColor;
     driverData["targetPosition"] = targetPosition;
     let targetRacingNumber;
     for (i in topThree) {
@@ -165,13 +269,11 @@ async function getAllDriverData(racingNumber) {
     if (targetDelta >= 0) {
         targetDelta = "+" + targetDelta;
     } else {
-        targetDeltaColor = "green";
+        targetDeltaColor = "#00ff00";
     }
     driverData["targetDelta"] = targetDelta;
     driverData["targetDeltaColor"] = targetDeltaColor;
     driverData["targetName"] =
-        driverList[targetRacingNumber].FirstName +
-        " " +
         driverList[targetRacingNumber].LastName.toUpperCase();
     let sector1 = getSectorInfo(racingNumber, 1, targetRacingNumber);
     let sector1time = sector1.sectorTime;
@@ -224,7 +326,7 @@ async function run() {
             console.log(bestTimes);
             console.log(sessionInfo);
         }
-        await sleep(10000);
+        await sleep(2000);
         apiRequests();
         getImprovedDriver();
     }
