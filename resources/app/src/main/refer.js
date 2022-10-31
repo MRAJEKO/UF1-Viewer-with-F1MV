a61db10f7ac282a2ff9e2262d333ef9f53b6fda0
MRAJEKO <aiden.kops@outlook.com>
2022-10-31 20:30:27 +0100

    Filter to only working windows

--- resources/app/src/main/refer.js
+++ resources/app/src/main/refer.js
@@ -1,12 +1,5 @@
 const { spawn, exec } = require("child_process");
 
-function launchMVF1() {
-    var mvPath =
-        "C:/Users/Aiden/AppData/Local/MultiViewerForF1/MultiViewer for F1.exe";
-    console.log("Launching MultiViewer");
-    spawn(mvPath, [], { detached: true });
-}
-
 function flagDisplay() {
     window.open(
         "../flagdisplay/index.html",
@@ -42,85 +35,10 @@ function singleRCM() {
     );
 }
 
-function trackInfo() {
-    window.open(
-        "../trackinfo/index.html",
-        "_blank",
-        "height=800px,width=1400px,transparent=true,frame=false,resizable=true,hasShadow=false"
-    );
-}
-
-function improve() {
-    window.open(
-        "../improves/index.html",
-        "_blank",
-        "height=500px,width=400px,transparent=true,frame=false,resizable=true,hasShadow=false"
-    );
-}
-
-function crashDetection() {
-    window.open(
-        "../crashDetection/index.html",
-        "_blank",
-        "height=400px,width=400px,transparent=true,frame=false,resizable=true,hasShadow=false"
-    );
-}
-
 function fastest() {
     window.open(
         "../fastest/index.html",
         "_blank",
         "height=300px,width=1000px,transparent=true,frame=false,resizable=true,hasShadow=false"
-    );
-}
-
-function FP() {
-    window.open(
-        "../flagdisplay/index.html",
-        "_blank",
-        "fullscreen=true,frame=false,nodeIntegration=no"
-    );
-    window.open(
-        "../fastest/index.html",
-        "_blank",
-        "height=251px,width=900px,transparent=true,frame=false,resizable=true,hasShadow=false"
-    );
-    window.open(
-        "../trackinfo/index.html",
-        "_blank",
-        "height=650px,width=950px,transparent=true,frame=false,resizable=true,hasShadow=false"
-    );
-    window.open(
-        "../crashDetection/index.html",
-        "_blank",
-        "height=400px,width=400px,transparent=true,frame=false,resizable=true,hasShadow=false"
-    );
-    window.open(
-        "../compass/index.html",
-        "_blank",
-        "height=80px,width=80px,transparent=true,frame=false,resizable=true,hasShadow=false"
-    );
-    window.open(
-        "../tracktime/index.html",
-        "_blank",
-        "height=80px,width=400px,transparent=true,frame=false,resizable=true,hasShadow=false,nodeIntegration=no"
-    );
-    window.open(
-        "../flagdisplay/index.html",
-        "_blank",
-        "height=100px,width=100px,frame=false,nodeIntegration=no"
-    );
-    window.open(
-        "../flagdisplay/index.html",
-        "_blank",
-        "height=100px,width=100px,frame=false,nodeIntegration=no"
-    );
-    let left = (window.screen.width - 1000) / 2;
-    window.open(
-        "../singlercm/index.html",
-        "_blank",
-        "top=19,left=" +
-            left +
-            ",alwaysOnTop=true,width=1000px,height=100px,frame=false,transparent=true,resizable=false,hasShadow=false,webPreferences={devTools=false}"
     );
 }
