da41b3f15c88a6c49883e2d9995321bd8c3c74b5
MRAJEKO <aiden.kops@outlook.com>
2022-11-04 15:50:53 +0100

    delete digiflag

--- src/digiflag/errorTable.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.failedToLoadAPI = exports.failedToCreateNewInstance = void 0;
-exports.failedToCreateNewInstance = new Error('An error occurred during the creation of the instance.');
-exports.failedToLoadAPI = new Error('You can\'t run more than 1 instance of the DigiFlag. Please use the "Create a new instance" button.\nIf there is no other instance open please contact us on the GitHub repo or on the F1MV server.');
--- src/digiflag/errorTable.ts
+++
@@ -1,4 +0,0 @@
-export const failedToCreateNewInstance = new Error('An error occurred during the creation of the instance.');
-export const failedToLoadAPI = new Error(
-    'You can\'t run more than 1 instance of the DigiFlag. Please use the "Create a new instance" button.\nIf there is no other instance open please contact us on the GitHub repo or on the F1MV server.'
-);
