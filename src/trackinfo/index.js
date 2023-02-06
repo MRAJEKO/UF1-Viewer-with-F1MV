e7f4be9905b8608232c4ee949ab6f41bae99bbfe
MRAJEKO <aiden.kops@outlook.com>
2023-02-06 12:48:11 +0100

    Remove Httprequest

--- src/trackinfo/index.js
+++ src/trackinfo/index.js
@@ -8,13 +8,6 @@ const sleep = (milliseconds) => {
     return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
 
-function httpGet(theUrl) {
-    let xmlHttpReq = new XMLHttpRequest();
-    xmlHttpReq.open("GET", theUrl, false);
-    xmlHttpReq.send(null);
-    return xmlHttpReq.responseText;
-}
-
 let transparent = false;
 function toggleBackground() {
     if (transparent) {
