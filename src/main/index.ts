62f1f1abf680d934fdaf25345e944ee2c65025ef
MRAJEKO <aiden.kops@outlook.com>
2023-03-24 23:10:51 +0100

    Remove nativeImage

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, shell, BrowserWindow, ipcMain, nativeImage } from 'electron'
+import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import Store from 'electron-store'
